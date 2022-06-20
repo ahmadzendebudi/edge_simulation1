c467d58b60c8932992a7795114e1de3cd9869c79
Ahmad <ahmadzendebudi@gmail.com>
2022-06-19 20:32:22 -0400

    ready for v3

--- run_simulator.py
+++ run_simulator.py
@@ -1,67 +1,55 @@
 from simulation_assist import SimulationAssist
 
 
-runIdentifier = "E"
+runIdentifier = "A"
 
 varients = []
 #varients.append({"step": 1, "config": "task_generator_lambda", "values": np.arange(0.1, 1.1, 0.1)})
 step = 0
 
+
+step += 1
+varients.append({"step": step, "config": "edge_selector", "values": ["dql"]})
+varients.append({"step": step, "config": "mobile_selector", "values": ["dql"]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.75]})
+
 """
 step += 1
 varients.append({"step": step, "config": "edge_selector", "values": ["dql"]})
 varients.append({"step": step, "config": "mobile_selector", "values": ["dql"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.75]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
+
+step += 1
+varients.append({"step": step, "config": "edge_selector", "values": ["dql"]})
+varients.append({"step": step, "config": "mobile_selector", "values": ["dql"]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
+
 
 step += 1
 varients.append({"step": step, "config": "edge_selector", "values": ["greedy"]})
 varients.append({"step": step, "config": "mobile_selector", "values": ["greedy"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.75]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
+
+step += 1
+varients.append({"step": step, "config": "edge_selector", "values": ["greedy"]})
+varients.append({"step": step, "config": "mobile_selector", "values": ["greedy"]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
 
 step += 1
 varients.append({"step": step, "config": "edge_selector", "values": ["local"]})
 varients.append({"step": step, "config": "mobile_selector", "values": ["random", "local", "remote"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.75]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
+
+step += 1
+varients.append({"step": step, "config": "edge_selector", "values": ["local"]})
+varients.append({"step": step, "config": "mobile_selector", "values": ["random", "local", "remote"]})
+varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
+varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
 """
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["dql"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["dql"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["dql"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["dql"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
-
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["greedy"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["greedy"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["greedy"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["greedy"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["local"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["random", "local", "remote"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [50, 75]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.25, 0.5, 0.75, 1]})
-
-step += 1
-varients.append({"step": step, "config": "edge_selector", "values": ["local"]})
-varients.append({"step": step, "config": "mobile_selector", "values": ["random", "local", "remote"]})
-varients.append({"step": step, "config": "boxworld_mobile_nodes", "values": [25, 100, 150]})
-varients.append({"step": step, "config": "task_generator_lambda", "values": [0.5, 0.75]})
-
 SimulationAssist.runBatchSimulation(varients, runIdentifier)
